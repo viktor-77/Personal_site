b059a23d664a9e73ec7ed1e8fec5ea13bcc66312
viktor-77 <vumanskiy0664041341@gmail.com>
2025-03-04 18:19:33 +0200

    Add a scroll-to-top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    // Close mobile menu after link click
     const menu_list = document.querySelector(".navbar-collapse");
 
     const hide_menu_list = () => {
@@ -8,4 +10,31 @@ document.addEventListener("DOMContentLoaded", function () {
     if (menu_list) {
         menu_list.addEventListener("click", () => setTimeout(hide_menu_list, 150));
     }
+
+    // Toogle top button
+    let scrollTop = document.querySelector('.scroll-top');
+    let footer = document.querySelector('footer');
+    let isFooterVisible = false;
+
+    function toggleScrollTop() {
+        if (scrollTop) {
+            if (window.scrollY > 200 && !isFooterVisible) {
+                scrollTop.classList.add('active');
+            } else {
+                scrollTop.classList.remove('active');
+            }
+        }
+    }
+
+    const footerObserver = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            isFooterVisible = entry.isIntersecting;
+            toggleScrollTop();
+        });
+    }, { threshold: 0 });
+
+    footerObserver.observe(footer);
+
+    window.addEventListener('load', toggleScrollTop);
+    document.addEventListener('scroll', toggleScrollTop);
 });
